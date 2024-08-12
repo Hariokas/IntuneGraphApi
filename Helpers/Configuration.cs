bf7d68cf78996c4148c17098b713f4db8e2245da
Haroldas Zalgevicius <HAZA@danskebank.lt>
2024-08-12 15:45:10 +0300

    ???

--- Helpers/Configuration.cs
+++ Helpers/Configuration.cs
@@ -5,4 +5,5 @@ public class Configuration
     public string TenantId { get; set; }
     public string ClientId { get; set; }
     public string ClientSecret { get; set; }
+    public string CertificateThumbprint { get; set; }
 }
